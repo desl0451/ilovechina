b23708ce5002b3e5f72634c4670171eace61d274
王涛 <desl0451@hotmail.com>
2018-10-25 15:08:29 +0800

    Revert "2018-10-25 日发起时划"
    This reverts commit d829e685b3ded72f2e595c82c9e7dff5495f1e80.

--- src/cn/ilovechina/interceptor/SysInterceptor.java
+++ src/cn/ilovechina/interceptor/SysInterceptor.java
@@ -25,9 +25,6 @@ public class SysInterceptor extends HandlerInterceptorAdapter {
 			response.sendRedirect(request.getContextPath() + "/statics/common/404.jsp");
 			return false;
 		}
-
-		logger.debug("---------------------------登录时加载网站信息-------------------------------");
-		
 		return true;
 	}
 }
